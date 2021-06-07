751ec8ebf735644a66dd15b044a11cb84320623a
No767 <ctg102b@gmail.com>
2021-06-07 16:57:11 -0700

    Add another button js function

--- hub/button1.js
+++ hub/button1.js
@@ -5,3 +5,7 @@ document.getElementById("button1").onclick = function () {
 document.getElementById("button2").onclick = function () {
     location.href = "https://maps.dcey.net/creative";
 };
+
+document.getElementById("button3").onclick = function () {
+    location.href = "https://maps.dcey.net/survival-v2";
+};
