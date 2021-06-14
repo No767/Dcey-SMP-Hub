2648f9034a647719a15a2dae362e57420f0d1e80
No767 <73260931+No767@users.noreply.github.com>
2021-06-13 17:23:25 -0700

    Add new assets

---
+++ hub/scripts/button-redirect.js
@@ -0,0 +1,11 @@
+document.getElementById("button1").onclick = function () {
+    location.href = "https://maps.dcey.net/survival";
+};
+
+document.getElementById("button2").onclick = function () {
+    location.href = "https://maps.dcey.net/creative";
+};
+
+document.getElementById("button3").onclick = function () {
+    location.href = "https://maps.dcey.net/survival-v2";
+};
